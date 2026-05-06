b304166b40128f4e250631f52350716b13b1b0e0
luciancardosolcd-gif <luciancardoso.lcd@gmail.com>
2026-05-06 11:23:36 -0300

    feat: projeto agroflow completo

---
+++ src/estoque/estoque.module.ts
@@ -0,0 +1,1 @@
+import{Module}from"@nestjs/common";import{TypeOrmModule}from"@nestjs/typeorm";import{Estoque}from"./estoque.entity";import{EstoqueService}from"./estoque.service";import{EstoqueController}from"./estoque.controller";@Module({imports:[TypeOrmModule.forFeature([Estoque])],controllers:[EstoqueController],providers:[EstoqueService],exports:[EstoqueService]})export class EstoqueModule{}

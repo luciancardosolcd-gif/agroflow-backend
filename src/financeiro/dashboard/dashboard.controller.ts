35edf67276fa39ee6a68d2b31e72ccfb6c56483f
luciancardosolcd-gif <luciancardoso.lcd@gmail.com>
2026-05-15 20:43:39 -0300

    Update dashboard.controller.ts

--- src/financeiro/dashboard/dashboard.controller.ts
+++ src/financeiro/dashboard/dashboard.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { ApiTags, ApiOperation, ApiBearerAuth } from '@nestjs/swagger';
 import { DashboardService } from './dashboard.service';
 import { FiltroDashboardDto } from '../dto/filtro-dashboard.dto';
-import { JwtAuthGuard } from '../../auth/jwt-auth.guard';
+import { RolesGuard } from '../../auth/roles.guard';
 
 @ApiTags('Financeiro - Dashboard')
 @ApiBearerAuth()

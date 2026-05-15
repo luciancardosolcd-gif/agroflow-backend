6046247608a329a6ff64f5a76e24a091ff22b2c3
luciancardosolcd-gif <luciancardoso.lcd@gmail.com>
2026-05-15 15:44:40 -0300

    Update financeiro.module.ts

--- src/financeiro/financeiro.module.ts
+++ src/financeiro/financeiro.module.ts
@@ -1,1 +1,16 @@
-import{Module}from"@nestjs/common";import{TypeOrmModule}from"@nestjs/typeorm";import{Financeiro}from"./financeiro.entity";import{FinanceiroService}from"./financeiro.service";import{FinanceiroController}from"./financeiro.controller";@Module({imports:[TypeOrmModule.forFeature([Financeiro])],controllers:[FinanceiroController],providers:[FinanceiroService],exports:[FinanceiroService]})export class FinanceiroModule{}
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Financeiro } from './financeiro.entity';
+import { FinanceiroService } from './financeiro.service';
+import { FinanceiroController } from './financeiro.controller';
+import { DashboardService } from './dashboard/dashboard.service';
+import { DashboardController } from './dashboard/dashboard.controller';
+ 
+@Module({
+  imports: [TypeOrmModule.forFeature([Financeiro])],
+  controllers: [FinanceiroController, DashboardController],
+  providers: [FinanceiroService, DashboardService],
+  exports: [FinanceiroService, DashboardService],
+})
+export class FinanceiroModule {}
+ 

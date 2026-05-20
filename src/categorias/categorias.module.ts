f066db7ab84a7f0ccfbbe5b6ce0403669a3827a4
luciancardosolcd-gif <luciancardoso.lcd@gmail.com>
2026-05-20 15:21:50 -0300

    Update categorias.module.ts

--- src/categorias/categorias.module.ts
+++ src/categorias/categorias.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { FinancialCategory } from './financial-category.entity';
+import { Financeiro } from '../financeiro/financeiro.entity';
 import { CategoriasService } from './categorias.service';
 import { CategoriasController } from './categorias.controller';
 import { RolesGuard } from '../auth/roles.guard';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([FinancialCategory])],
+  imports: [TypeOrmModule.forFeature([FinancialCategory, Financeiro])],
   controllers: [CategoriasController],
   providers: [CategoriasService, RolesGuard],
   exports: [CategoriasService],

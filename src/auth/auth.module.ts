42e0af7930737e14b2415cb120758f8652cfa01f
luciancardosolcd-gif <luciancardoso.lcd@gmail.com>
2026-05-20 10:51:29 -0300

    Update auth.module.ts

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,1 +1,25 @@
-import{Module}from"@nestjs/common";import{JwtModule}from"@nestjs/jwt";import{PassportModule}from"@nestjs/passport";import{ConfigModule,ConfigService}from"@nestjs/config";import{TypeOrmModule}from"@nestjs/typeorm";import{AuthController}from"./auth.controller";import{AuthService}from"./auth.service";import{JwtStrategy}from"./jwt.strategy";import{User}from"../users/user.entity";@Module({imports:[PassportModule,TypeOrmModule.forFeature([User]),JwtModule.registerAsync({imports:[ConfigModule],useFactory:(config:ConfigService)=>({secret:config.get("JWT_SECRET"),signOptions:{expiresIn:"8h"}}),inject:[ConfigService]})],controllers:[AuthController],providers:[AuthService,JwtStrategy],exports:[AuthService]})export class AuthModule{}
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt.strategy';
+import { User } from '../users/user.entity';
+
+const JWT_SECRET = process.env.JWT_SECRET || 'fallback-secret-key';
+
+@Module({
+  imports: [
+    PassportModule,
+    TypeOrmModule.forFeature([User]),
+    JwtModule.register({
+      secret: JWT_SECRET,
+      signOptions: { expiresIn: '8h' },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, JwtStrategy],
+  exports: [AuthService],
+})
+export class AuthModule {}

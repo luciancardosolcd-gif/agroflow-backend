b24733c77920a0b79e0f0aa9a34b82ed0e4b6b13
luciancardosolcd-gif <luciancardoso.lcd@gmail.com>
2026-05-22 00:16:07 -0300

    Update financeiro.service.ts

--- src/financeiro/financeiro.service.ts
+++ src/financeiro/financeiro.service.ts
@@ -1,1 +1,35 @@
-import{Injectable}from"@nestjs/common";import{InjectRepository}from"@nestjs/typeorm";import{Repository}from"typeorm";import{Financeiro}from"./financeiro.entity";@Injectable()export class FinanceiroService{constructor(@InjectRepository(Financeiro)private repo:Repository<Financeiro>){}findAll(){return this.repo.find();}findOne(id:string){return this.repo.findOne({where:{id}});}create(data:Partial<Financeiro>){return this.repo.save(data);}update(id:string,data:Partial<Financeiro>){return this.repo.update(id,data);}remove(id:string){return this.repo.delete(id);}}
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Financeiro } from './financeiro.entity';
+
+@Injectable()
+export class FinanceiroService {
+  constructor(
+    @InjectRepository(Financeiro)
+    private repo: Repository<Financeiro>
+  ) {}
+
+  findAll() {
+    return this.repo.find();
+  }
+
+  findOne(id: string) {
+    return this.repo.findOne({ where: { id } });
+  }
+
+  create(data: Partial<Financeiro>) {
+    return this.repo.save(data);
+  }
+
+  async update(id: string, data: Partial<Financeiro>) {
+    const existing = await this.repo.findOne({ where: { id } });
+    if (!existing) throw new Error('Lançamento não encontrado');
+    const updated = this.repo.merge(existing, data);
+    return this.repo.save(updated);
+  }
+
+  remove(id: string) {
+    return this.repo.delete(id);
+  }
+}

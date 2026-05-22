16595f519f36ffe21e1488547b93c2d9b44ef2e5
luciancardosolcd-gif <luciancardoso.lcd@gmail.com>
2026-05-22 16:00:01 -0300

    Update propriedades.service.ts

--- src/propriedades/propriedades.service.ts
+++ src/propriedades/propriedades.service.ts
@@ -1,1 +1,31 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Propriedade } from './propriedade.entity';
 
+@Injectable()
+export class PropriedadesService {
+  constructor(
+    @InjectRepository(Propriedade)
+    private readonly repo: Repository<Propriedade>,
+  ) {}
+
+  findAll() {
+    return this.repo.find({ order: { nome: 'ASC' } });
+  }
+
+  async findOne(id: string) {
+    const prop = await this.repo.findOne({ where: { id } });
+    if (!prop) throw new NotFoundException('Propriedade não encontrada');
+    return prop;
+  }
+
+  create(data: Partial<Propriedade>) {
+    const prop = this.repo.create(data);
+    return this.repo.save(prop);
+  }
+
+  async update(id: string, data: Partial<Propriedade>) {
+    const prop = await this.findOne(id);
+    const updated = this.repo.merge(prop, data);
+    return

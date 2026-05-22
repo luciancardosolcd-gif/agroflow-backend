709f6325f85fed5ec33bdac7ec469ebf359a95cc
luciancardosolcd-gif <luciancardoso.lcd@gmail.com>
2026-05-22 16:00:42 -0300

    Update propriedades.controller.ts

--- src/propriedades/propriedades.controller.ts
+++ src/propriedades/propriedades.controller.ts
@@ -1,1 +1,18 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Param,
+  UseGuards,
+} from '@nestjs/common';
+import { ApiTags, ApiBearerAuth } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
+import { RolesGuard, Roles } from '../auth/roles.guard';
+import { PropriedadesService } from './propriedades.service';
 
+@ApiTags('Propriedades')
+@ApiBearerAuth()
+@UseGuards(AuthGuard('jwt'), Roles

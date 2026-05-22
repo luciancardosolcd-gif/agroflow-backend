67ebfb17d8cd4e6fce7a18ea5df3a605380ff9bf
luciancardosolcd-gif <luciancardoso.lcd@gmail.com>
2026-05-22 16:02:50 -0300

    Update propriedades.module.ts

--- src/propriedades/propriedades.module.ts
+++ src/propriedades/propriedades.module.ts
@@ -1,1 +1,13 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Propriedade } from './propriedade.entity';
+import { PropriedadesService } from './propriedades.service';
+import { PropriedadesController } from './propriedades.controller';
 
+@Module({
+  imports: [TypeOrmModule.forFeature([Propriedade])],
+  controllers: [PropriedadesController],
+  providers: [PropriedadesService],
+  exports: [PropriedadesService],
+})
+export class PropriedadesModule {}

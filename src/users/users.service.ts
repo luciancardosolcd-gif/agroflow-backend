14e984c90a8f7fe951d8521703b11fc732142103
luciancardosolcd-gif <luciancardoso.lcd@gmail.com>
2026-05-23 00:41:07 -0300

    Update users.service.ts

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,1 +1,35 @@
-import{Injectable}from"@nestjs/common";import{InjectRepository}from"@nestjs/typeorm";import{Repository}from"typeorm";import{User}from"./user.entity";@Injectable()export class UsersService{constructor(@InjectRepository(User)private repo:Repository<User>){}findAll(){return this.repo.find();}findOne(id:string){return this.repo.findOne({where:{id}});}create(data:Partial<User>){return this.repo.save(data);}update(id:string,data:Partial<User>){return this.repo.update(id,data);}remove(id:string){return this.repo.delete(id);}}
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private repo: Repository<User>,
+  ) {}
+
+  findAll() {
+    return this.repo.find();
+  }
+
+  findOne(id: string) {
+    return this.repo.findOne({ where: { id } });
+  }
+
+  create(data: Partial<User>) {
+    return this.repo.save(data);
+  }
+
+  async update(id: string, data: Partial<User>) {
+    const user = await this.repo.findOne({ where: { id } });
+    if (!user) throw new Error('Usuário não encontrado');
+    const merged = this.repo.merge(user, data);
+    return this.repo.save(merged);
+  }
+
+  remove(id: string) {
+    return this.repo.delete(id);
+  }
+}

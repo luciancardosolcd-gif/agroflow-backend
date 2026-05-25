c11c70a23e21ff15fbf2a1ecd824e9542cc93511
luciancardosolcd-gif <luciancardoso.lcd@gmail.com>
2026-05-25 17:49:51 -0300

    Update safras.module.ts

--- src/safras/safras.module.ts
+++ src/safras/safras.module.ts
@@ -3,9 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Safra } from './safra.entity';
 import { SafrasService } from './safras.service';
 import { SafrasController } from './safras.controller';
+import { User } from '../users/user.entity';
+import { Propriedade } from '../propriedades/propriedade.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Safra])],
+  imports: [TypeOrmModule.forFeature([Safra, User, Propriedade])],
   controllers: [SafrasController],
   providers: [SafrasService],
   exports: [SafrasService],

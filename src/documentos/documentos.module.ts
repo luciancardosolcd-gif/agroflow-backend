956cc0bf4ff5d66fe53329e52043b1abec8a4144
luciancardosolcd-gif <luciancardoso.lcd@gmail.com>
2026-05-25 23:14:25 -0300

    Update documentos.module.ts

--- src/documentos/documentos.module.ts
+++ src/documentos/documentos.module.ts
@@ -1,1 +1,14 @@
-import{Module}from"@nestjs/common";import{TypeOrmModule}from"@nestjs/typeorm";import{Documento}from"./documento.entity";import{DocumentosService}from"./documentos.service";import{DocumentosController}from"./documentos.controller";@Module({imports:[TypeOrmModule.forFeature([Documento])],controllers:[DocumentosController],providers:[DocumentosService],exports:[DocumentosService]})export class DocumentosModule{}
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Documento } from './documento.entity';
+import { DocumentosService } from './documentos.service';
+import { DocumentosController } from './documentos.controller';
+import { PermissionsGuard } from '../auth/permissions.guard';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Documento])],
+  controllers: [DocumentosController],
+  providers: [DocumentosService, PermissionsGuard],
+  exports: [DocumentosService],
+})
+export class DocumentosModule {}

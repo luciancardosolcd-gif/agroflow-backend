743540d490f742d90da404fa24cdf4e6d4c1cddf
luciancardosolcd-gif <luciancardoso.lcd@gmail.com>
2026-05-25 23:20:39 -0300

    Update estoque.module.ts

--- src/estoque/estoque.module.ts
+++ src/estoque/estoque.module.ts
@@ -4,9 +4,10 @@ import { Estoque } from './estoque.entity';
 import { EstoqueService } from './estoque.service';
 import { EstoqueController } from './estoque.controller';
 import { PermissionsGuard } from '../auth/permissions.guard';
+import { User } from '../users/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Estoque])],
+  imports: [TypeOrmModule.forFeature([Estoque, User])],
   controllers: [EstoqueController],
   providers: [EstoqueService, PermissionsGuard],
   exports: [EstoqueService],

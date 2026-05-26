3a8c5e02c4b3bbcb890ca9ed2d232cddd2875a20
luciancardosolcd-gif <luciancardoso.lcd@gmail.com>
2026-05-25 23:13:01 -0300

    Update clientes.module.ts

--- src/clientes/clientes.module.ts
+++ src/clientes/clientes.module.ts
@@ -1,1 +1,14 @@
-import{Module}from"@nestjs/common";import{TypeOrmModule}from"@nestjs/typeorm";import{Cliente}from"./cliente.entity";import{ClientesService}from"./clientes.service";import{ClientesController}from"./clientes.controller";@Module({imports:[TypeOrmModule.forFeature([Cliente])],controllers:[ClientesController],providers:[ClientesService],exports:[ClientesService]})export class ClientesModule{}
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Cliente } from './cliente.entity';
+import { ClientesService } from './clientes.service';
+import { ClientesController } from './clientes.controller';
+import { PermissionsGuard } from '../auth/permissions.guard';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Cliente])],
+  controllers: [ClientesController],
+  providers: [ClientesService, PermissionsGuard],
+  exports: [ClientesService],
+})
+export class ClientesModule {}

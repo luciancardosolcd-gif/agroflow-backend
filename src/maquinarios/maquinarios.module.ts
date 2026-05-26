d4675bddf06b800c91cd471ce8635afb86e74b23
luciancardosolcd-gif <luciancardoso.lcd@gmail.com>
2026-05-25 23:22:01 -0300

    Update maquinarios.module.ts

--- src/maquinarios/maquinarios.module.ts
+++ src/maquinarios/maquinarios.module.ts
@@ -4,9 +4,10 @@ import { Maquinario } from './maquinario.entity';
 import { MaquinariosService } from './maquinarios.service';
 import { MaquinariosController } from './maquinarios.controller';
 import { PermissionsGuard } from '../auth/permissions.guard';
+import { User } from '../users/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Maquinario])],
+  imports: [TypeOrmModule.forFeature([Maquinario, User])],
   controllers: [MaquinariosController],
   providers: [MaquinariosService, PermissionsGuard],
   exports: [MaquinariosService],

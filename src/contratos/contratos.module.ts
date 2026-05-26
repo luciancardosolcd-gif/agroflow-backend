0240c6c01c6b11e84190cba7ccb8fa6fe19f1d6e
luciancardosolcd-gif <luciancardoso.lcd@gmail.com>
2026-05-25 23:13:18 -0300

    Update contratos.module.ts

--- src/contratos/contratos.module.ts
+++ src/contratos/contratos.module.ts
@@ -1,1 +1,15 @@
-import{Module}from"@nestjs/common";import{TypeOrmModule}from"@nestjs/typeorm";import{Contrato}from"./contrato.entity";import{ContratosService}from"./contratos.service";import{ContratosController}from"./contratos.controller";@Module({imports:[TypeOrmModule.forFeature([Contrato])],controllers:[ContratosController],providers:[ContratosService],exports:[ContratosService]})export class ContratosModule{}
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Contrato } from './contrato.entity';
+import { ContratosService } from './contratos.service';
+import { ContratosController } from './contratos.controller';
+import { PermissionsGuard } from '../auth/permissions.guard';
+import { RolesGuard } from '../auth/roles.guard';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Contrato])],
+  controllers: [ContratosController],
+  providers: [ContratosService, PermissionsGuard, RolesGuard],
+  exports: [ContratosService],
+})
+export class ContratosModule {}

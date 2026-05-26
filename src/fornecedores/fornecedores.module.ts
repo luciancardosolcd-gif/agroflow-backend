01781d4bf27af1132e610a0f97a3002c11e3d8ac
luciancardosolcd-gif <luciancardoso.lcd@gmail.com>
2026-05-25 23:21:43 -0300

    Update fornecedores.module.ts

--- src/fornecedores/fornecedores.module.ts
+++ src/fornecedores/fornecedores.module.ts
@@ -4,9 +4,10 @@ import { Fornecedor } from './fornecedor.entity';
 import { FornecedoresService } from './fornecedores.service';
 import { FornecedoresController } from './fornecedores.controller';
 import { PermissionsGuard } from '../auth/permissions.guard';
+import { User } from '../users/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Fornecedor])],
+  imports: [TypeOrmModule.forFeature([Fornecedor, User])],
   controllers: [FornecedoresController],
   providers: [FornecedoresService, PermissionsGuard],
   exports: [FornecedoresService],

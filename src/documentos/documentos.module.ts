eab98414c6c30b741b7047c242bb001a7677afb1
luciancardosolcd-gif <luciancardoso.lcd@gmail.com>
2026-05-25 23:22:14 -0300

    Update documentos.module.ts

--- src/documentos/documentos.module.ts
+++ src/documentos/documentos.module.ts
@@ -4,9 +4,10 @@ import { Documento } from './documento.entity';
 import { DocumentosService } from './documentos.service';
 import { DocumentosController } from './documentos.controller';
 import { PermissionsGuard } from '../auth/permissions.guard';
+import { User } from '../users/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Documento])],
+  imports: [TypeOrmModule.forFeature([Documento, User])],
   controllers: [DocumentosController],
   providers: [DocumentosService, PermissionsGuard],
   exports: [DocumentosService],

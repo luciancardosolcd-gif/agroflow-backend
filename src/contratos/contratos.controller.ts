122c2db164edf490dcedc1eb383d0114dafe28c6
luciancardosolcd-gif <luciancardoso.lcd@gmail.com>
2026-05-25 23:06:30 -0300

    Update contratos.controller.ts

--- src/contratos/contratos.controller.ts
+++ src/contratos/contratos.controller.ts
@@ -1,1 +1,35 @@
-import{Controller,Get,Post,Put,Delete,Body,Param,UseGuards}from"@nestjs/common";import{ApiTags,ApiBearerAuth}from"@nestjs/swagger";import{AuthGuard}from"@nestjs/passport";import{ContratosService}from"./contratos.service";@ApiTags("Contratos")@ApiBearerAuth()@UseGuards(AuthGuard("jwt"))@Controller("contratos")export class ContratosController{constructor(private service:ContratosService){}@Get()findAll(){return this.service.findAll();}@Get(":id")findOne(@Param("id")id:string){return this.service.findOne(id);}@Post()create(@Body()data:any){return this.service.create(data);}@Put(":id")update(@Param("id")id:string,@Body()data:any){return this.service.update(id,data);}@Delete(":id")remove(@Param("id")id:string){return this.service.remove(id);}}
+import { Controller, Get, Post, Put, Delete, Body, Param, UseGuards } from '@nestjs/common';
+import { ApiTags, ApiBearerAuth } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
+import { ContratosService } from './contratos.service';
+import { PermissionsGuard, RequirePermission } from '../auth/permissions.guard';
+import { RolesGuard } from '../auth/roles.guard';
+
+@ApiTags('Contratos')
+@ApiBearerAuth()
+@UseGuards(AuthGuard('jwt'), RolesGuard)
+@Controller('contratos')
+export class ContratosController {
+  constructor(private service: ContratosService) {}
+
+  @Get()
+  findAll() { return this.service.findAll(); }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) { return this.service.findOne(id); }
+
+  @Post()
+  @UseGuards(AuthGuard('jwt'), RolesGuard, PermissionsGuard)
+  @RequirePermission('contratos', 'criar')
+  create(@Body() data: any) { return this.service.create(data); }
+
+  @Put(':id')
+  @UseGuards(AuthGuard('jwt'), RolesGuard, PermissionsGuard)
+  @RequirePermission('contratos', 'editar')
+  update(@Param('id') id: string, @Body() data: any) { return this.service.update(id, data); }
+
+  @Delete(':id')
+  @UseGuards(AuthGuard('jwt'), RolesGuard, PermissionsGuard)
+  @RequirePermission('contratos', 'deletar')
+  remove(@Param('id') id: string) { return this.service.remove(id); }
+}

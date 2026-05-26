c73578997f828076a57b94e3f671938fbc406e24
luciancardosolcd-gif <luciancardoso.lcd@gmail.com>
2026-05-25 22:54:40 -0300

    Update financeiro.module.ts

--- src/financeiro/financeiro.module.ts
+++ src/financeiro/financeiro.module.ts
@@ -7,11 +7,12 @@ import { DashboardService } from './dashboard/dashboard.service';
 import { DashboardController } from './dashboard/dashboard.controller';
 import { User } from '../users/user.entity';
 import { Propriedade } from '../propriedades/propriedade.entity';
+import { PermissionsGuard } from '../auth/permissions.guard';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Financeiro, User, Propriedade])],
   controllers: [FinanceiroController, DashboardController],
-  providers: [FinanceiroService, DashboardService],
+  providers: [FinanceiroService, DashboardService, PermissionsGuard],
   exports: [FinanceiroService, DashboardService],
 })
 export class FinanceiroModule {}

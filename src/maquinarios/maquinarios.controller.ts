7039727771e7d8901a4ca2168004ebdcf2e824f6
luciancardosolcd-gif <luciancardoso.lcd@gmail.com>
2026-05-26 21:11:36 -0300

    Update maquinarios.controller.ts

--- src/maquinarios/maquinarios.controller.ts
+++ src/maquinarios/maquinarios.controller.ts
@@ -3,7 +3,6 @@ import { ApiTags, ApiBearerAuth } from '@nestjs/swagger';
 import { AuthGuard } from '@nestjs/passport';
 import { MaquinariosService } from './maquinarios.service';
 import { RolesGuard, Roles } from '../auth/roles.guard';
-import { PermissionsGuard, RequirePermission } from '../auth/permissions.guard';
 
 @ApiTags('Maquinarios')
 @ApiBearerAuth()
@@ -20,19 +19,13 @@ export class MaquinariosController {
 
   @Post()
   @Roles('admin', 'gestor', 'operador')
-  @UseGuards(AuthGuard('jwt'), RolesGuard, PermissionsGuard)
-  @RequirePermission('maquinarios', 'criar')
   create(@Body() data: any) { return this.service.create(data); }
 
   @Put(':id')
-  @Roles('admin', 'gestor')
-  @UseGuards(AuthGuard('jwt'), RolesGuard, PermissionsGuard)
-  @RequirePermission('maquinarios', 'editar')
+  @Roles('admin', 'gestor', 'operador')
   update(@Param('id') id: string, @Body() data: any) { return this.service.update(id, data); }
 
   @Delete(':id')
   @Roles('admin')
-  @UseGuards(AuthGuard('jwt'), RolesGuard, PermissionsGuard)
-  @RequirePermission('maquinarios', 'deletar')
   remove(@Param('id') id: string) { return this.service.remove(id); }
 }

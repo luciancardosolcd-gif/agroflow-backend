94527a38209af23756bc2f97bdf664e2b3b45a67
luciancardosolcd-gif <luciancardoso.lcd@gmail.com>
2026-07-01 16:19:38 -0300

    Update talhoes.module.ts

--- src/talhoes/talhoes.module.ts
+++ src/talhoes/talhoes.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Talhao } from './talhoes.entity';
+import { Talhao } from './talhao.entity';
 import { TalhoesService } from './talhoes.service';
 import { TalhoesController } from './talhoes.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Talhao])],
-  providers: [TalhoesService],
   controllers: [TalhoesController],
+  providers: [TalhoesService],
 })
 export class TalhoesModule {}

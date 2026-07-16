c6e657b59b0464944003f650bbb91acfee673b71
luciancardosolcd-gif <luciancardoso.lcd@gmail.com>
2026-07-16 16:46:05 -0300

    Update entity.ts

--- src/registros climatico/entity.ts
+++ src/registros climatico/entity.ts
@@ -1,1 +1,31 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn } from 'typeorm'
 
+@Entity('registros_climaticos')
+export class RegistroClimatico {
+  @PrimaryGeneratedColumn('uuid')
+  id: string
+
+  @Column({ nullable: true })
+  propriedade_id: string
+
+  @Column({ nullable: true })
+  estacao: string
+
+  @Column({ type: 'date' })
+  data: string
+
+  @Column({ type: 'float', nullable: true })
+  precipitacao: number
+
+  @Column({ type: 'float', nullable: true })
+  temperatura: number
+
+  @Column({ type: 'float', nullable: true })
+  umidade: number
+
+  @Column({ type: 'float', nullable: true })
+  pressao: number
+
+  @CreateDateColumn()
+  created_at: Date
+}
